fcc09e88f9c8352c554f4355df234468cecbe86a
Mateusz <matmudry@gmail.com>
2024-09-02 00:22:08 +0200

    ul and li elements changed for div elements

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-const button = document.querySelector("button");
+const button = document.querySelector('button');
 
 let listCount = 0;
 let taskCount = 0;
 
-const createTaskList = function () {
-  const taskList = document.createElement("ul");
-  document.querySelector("main").appendChild(taskList);
+// const createTaskList = function () {
+//   const taskList = document.createElement("ul");
+//   document.querySelector("main").appendChild(taskList);
+//   taskList.classList.add(`taskList${listCount}`);
+//   const addTaskButton = document.createElement("button");
+//   addTaskButton.textContent = "Dodaj zadanie";
+//   document.querySelector(`.taskList${listCount}`).appendChild(addTaskButton);
+//   // document.querySelector("ul").parentElement.appendChild(addTaskButton);
+//   const createTask = function () {
+//     const taskItem = document.createElement("li");
+//     taskItem.textContent = `Zadanie ${taskCount}`;
+//     // document.querySelector("ul").parentElement.appendChild(taskItem);
+//     taskList.appendChild(taskItem);
+//     taskCount++;
+//   };
+//   addTaskButton.addEventListener("click", createTask);
+//   listCount++;
+//   console.log(listCount);
+// };
+
+function createTaskList() {
+  const taskList = document.createElement('div');
   taskList.classList.add(`taskList${listCount}`);
-  const addTaskButton = document.createElement("button");
-  addTaskButton.textContent = "Dodaj zadanie";
-  document.querySelector(`.taskList${listCount}`).appendChild(addTaskButton);
-  // document.querySelector("ul").parentElement.appendChild(addTaskButton);
-  const createTask = function () {
-    const taskItem = document.createElement("li");
-    taskItem.textContent = `Zadanie ${taskCount}`;
-    // document.querySelector("ul").parentElement.appendChild(taskItem);
-    taskList.appendChild(taskItem);
-    taskCount++;
-  };
-  addTaskButton.addEventListener("click", createTask);
+  taskList.classList.toggle('taskListDiv');
+  document.querySelector('main').appendChild(taskList);
+
+  const addTaskButton = addTask(taskList);
+  taskList.appendChild(addTaskButton);
+
   listCount++;
-  console.log(listCount);
-};
+}
 
-button.addEventListener("click", createTaskList);
+function addTask(taskList) {
+  const button = document.createElement('button');
+  button.textContent = 'Dodaj zadanie';
+  button.addEventListener('click', () => createTask(taskList));
+  return button;
+}
 
-console.log(listCount);
+function createTask(taskList) {
+  const taskItem = document.createElement('div');
+  taskItem.classList.toggle('taskDiv');
+  taskItem.textContent = `Zadanie ${taskCount}`;
+  taskList.appendChild(taskItem);
+  taskCount++;
+  const checkboxItem = document.createElement('input');
+  checkboxItem.type = 'checkbox';
+  taskItem.appendChild(checkboxItem);
+}
+
+button.addEventListener('click', createTaskList);
+
+// console.log(listCount);
